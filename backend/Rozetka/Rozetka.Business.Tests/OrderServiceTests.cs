bff49c23387f1d3c7a8f82428e4dad393435afa9
Ivanashko <ivanashko.d2801.122.24@lntu.edu.ua>
2025-11-26 17:45:52 +0200

    Tests for order are ready

--- backend/Rozetka/Rozetka.Business.Tests/OrderServiceTests.cs
+++ backend/Rozetka/Rozetka.Business.Tests/OrderServiceTests.cs
@@ -33,6 +33,65 @@ public class OrderServiceTests
         Assert.That(result.Number, Is.EqualTo(number));
         Assert.That(result.Id, Is.Not.EqualTo(Guid.Empty));
     }
+
+    [Test]
+    public async Task Update_Order()
+    {
+        //Arrange
+        var id = Guid.NewGuid();
+        var existingOrder = new Order
+        {
+            Id = id,
+            Status = "New",
+            Total = 100,
+        };
+        _mockRepo.Setup(x => x.GetById(id)).ReturnsAsync(existingOrder);
+        
+        //Act
+        await _service.Update(id, "Paid", 200);
+        //Assert
+        _mockRepo.Verify(x => x.Update(existingOrder), Times.Once);
+        Assert.That(existingOrder.Status, Is.EqualTo("Paid"));
+        Assert.That(existingOrder.Total, Is.EqualTo(200));
+    }
+
+    [Test]
+    public async Task Delete_Order()
+    {
+        //Arrange
+        var id = Guid.NewGuid();
+        //Act
+        await _service.Delete(id);
+        //Assert
+        _mockRepo.Verify(x => x.Delete(id), Times.Once);
+    }
+
+    [Test]
+    public async Task GetAll_Orders()
+    {
+        //Arrange
+        var fakeList = new List<Order> { new Order(),  new Order() };
+        _mockRepo.Setup(x => x.GetAll()).ReturnsAsync(fakeList);
+        //Act
+        var result = await _service.GetAll();
+        //Assert
+        Assert.That(result.Count(), Is.EqualTo(fakeList.Count));
+    }
+
+    [Test]
+    public async Task GetById_Order()
+    {
+        //Arrange
+        var id = Guid.NewGuid();
+        var order = new Order{Id = id};
+        _mockRepo.Setup(x => x.GetById(id)).ReturnsAsync(order);
+        //Act
+        var result = await _service.GetById(id);
+        //Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Id, Is.EqualTo(order.Id));
+
+    }
 }
 //Arrange
 //Act
